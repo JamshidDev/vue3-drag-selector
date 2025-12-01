fed697627a14a95e1be21b126561fc3f381aad28
JAMACODER <64282052+JamshidDev@users.noreply.github.com>
2025-12-01 19:00:05 +0500

    fixed version bugs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Vue3DragSelector from "vue3-drag-selector"
 
 const app = createApp(App)
-app.use(Vue3DragSelector)
 app.mount('#app')
